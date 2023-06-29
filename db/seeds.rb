a362fbdc581f2ea09d0b96d856a94bbcd8e6d6cc
Paweł Kwietniewski <kniarzyn@gmail.com>
2023-06-29 18:55:59 +0200

    Add new fileds to Movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+  [
+  {title: "Batman",
+  rating: "PG",
+  total_gross: 0.125e6,
+  description: "Batman hinting bad boys",
+  released_on: "2022-03-20"},
+  {title: "Robin Hood",
+  rating: "PG11",
+  total_gross: 0.75e5,
+  description: "Robin Hood and his funny team own teh forest",
+  released_on: "2010-01-10"},
+  {title: "IrinMan",
+  rating: "PG15",
+  total_gross: 0.1015e6,
+  description: "What this film is all about?",
+  released_on: "2019-01-01"}
+].each {|movie| Movie.create(movie)}
