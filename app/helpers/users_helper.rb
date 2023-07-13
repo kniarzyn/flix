8c94bbaedbde6b1d392b8f48cddc09152aec75ee
kniarzyn <kniarzyn@gmail.com>
2023-07-13 19:37:08 +0200

    Add gravatar in user#show

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,9 @@ module UsersHelper
   def member_since(user)
     user.created_at.strftime('%B %Y')
   end
+
+  def profile_image(user)
+    url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}"
+    image_tag(url, alt: user.name)
+  end
 end
